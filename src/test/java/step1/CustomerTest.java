d532c299352dbf22f6df3b9723c8907b385626bf
Ian <developer.kss@gmail.com>
2018-04-13 07:51:44 +0900

    test2

--- src/test/java/step1/CustomerTest.java
+++ src/test/java/step1/CustomerTest.java
@@ -18,4 +18,18 @@ public class CustomerTest {
 
     }
 
+    @Test
+    public void test2() throws Exception{
+        String name = null;
+        Customer customer = new Customer(name);
+
+        Movie movie = null;
+        int daysRented = 0;
+        Rental rental = new Rental(movie, daysRented);
+        customer.addRental(rental);
+        assertEquals("Rental Record for null\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter pointers", customer.statement());
+
+    }
 }
