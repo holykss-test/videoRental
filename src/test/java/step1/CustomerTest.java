a145fa4429e9db6b3673c6f35327363ac768598c
Ian <developer.kss@gmail.com>
2018-04-13 07:56:33 +0900

    test for Regular 3 days

--- src/test/java/step1/CustomerTest.java
+++ src/test/java/step1/CustomerTest.java
@@ -8,7 +8,7 @@ public class CustomerTest {
 
 
     @Test
-    public void test1() throws Exception{
+    public void testForEmptyRentals() throws Exception{
         String name = null;
         Customer customer = new Customer(name);
 
@@ -19,19 +19,37 @@ public class CustomerTest {
     }
 
     @Test
-    public void test2() throws Exception{
+    public void testRegularMovieFor2Days() throws Exception{
         String name = null;
         Customer customer = new Customer(name);
 
-        int priceCode = 0;
+        int priceCode = Movie.REGULAR;
         String title = null;
         Movie movie = new Movie(title, priceCode);
-        int daysRented = 0;
+        int daysRented = 2;
         Rental rental = new Rental(movie, daysRented);
         customer.addRental(rental);
         assertEquals("Rental Record for null\n" +
                 "\t2.0(null)\n" +
                 "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter pointers", customer.statement());
+
+    }
+
+    @Test
+    public void testRegularMovieFor3Days() throws Exception{
+        String name = null;
+        Customer customer = new Customer(name);
+
+        int priceCode = Movie.REGULAR;
+        String title = null;
+        Movie movie = new Movie(title, priceCode);
+        int daysRented = 3;
+        Rental rental = new Rental(movie, daysRented);
+        customer.addRental(rental);
+        assertEquals("Rental Record for null\n" +
+                "\t3.5(null)\n" +
+                "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter pointers", customer.statement());
 
     }
