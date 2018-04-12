d3f7d29d1c279185de1f0689eddbff8ea0e6dfff
Ian <developer.kss@gmail.com>
2018-04-12 15:26:15 +0900

    변수화

--- src/main/java/step1/Rental.java
+++ src/main/java/step1/Rental.java
@@ -21,21 +21,23 @@ class Rental {
 		// determine amounts for each line
 
 		double thisAmount = 0;
+		final int daysRented = getDaysRented();
+
 		switch (getMovie().getPriceCode()) {
         case Movie.REGULAR:
             thisAmount += 2;
-            if (getDaysRented() > 2)
-                thisAmount += (getDaysRented() - 2) * 1.5;
+            if (daysRented > 2)
+                thisAmount += (daysRented - 2) * 1.5;
             break;
 
         case Movie.NEW_RELEASE:
-            thisAmount += getDaysRented() * 3;
+            thisAmount += daysRented * 3;
             break;
 
         case Movie.CHILDRENS:
             thisAmount += 1.5;
-            if (getDaysRented() > 3)
-                thisAmount += (getDaysRented() - 3) * 1.5;
+            if (daysRented > 3)
+                thisAmount += (daysRented - 3) * 1.5;
             break;
         }
 		return thisAmount;
