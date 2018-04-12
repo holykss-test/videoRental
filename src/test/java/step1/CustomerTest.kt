0a0bd57410f7ace45a9c04add593eb881c78aa0b
Ian <developer.kss@gmail.com>
2018-04-12 13:11:04 +0900

    CustomerTest

---
+++ src/test/java/step1/CustomerTest.kt
@@ -0,0 +1,14 @@
+package step1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CustomerTest {
+
+    @Test
+    fun testCreate() {
+        val name = null
+        val customer = Customer(name)
+    }
+}
