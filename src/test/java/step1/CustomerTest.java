632a709b0a3144c0fcd8b5d581178d7ababb5cd7
Ian <developer.kss@gmail.com>
2018-04-13 07:48:44 +0900

    assertEquals(null, customer.statement());

--- src/test/java/step1/CustomerTest.java
+++ src/test/java/step1/CustomerTest.java
@@ -9,6 +9,10 @@ public class CustomerTest {
 
     @Test
     public void test1() throws Exception{
+        String name = null;
+        Customer customer = new Customer(name);
+
+        assertEquals(null, customer.statement());
 
     }
 
