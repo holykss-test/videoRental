95e76042bb04df33b0df47c4f84298a7c9e5a0b9
Ian <developer.kss@gmail.com>
2018-04-12 13:32:01 +0900

    test2

--- src/test/java/step1/CustomerTest.kt
+++ src/test/java/step1/CustomerTest.kt
@@ -7,8 +7,30 @@ import org.junit.Assert.*
 class CustomerTest {
 
     @Test
-    fun testCreate() {
+    fun 익명테스트1() {
         val name = null
         val customer = Customer(name)
+
+        val statement = customer.statement()
+        assertEquals("Rental Record for null\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter pointers", statement)
+    }
+
+    @Test
+    fun 익명테스트2() {
+        val name = null
+        val customer = Customer(name)
+
+        val rental = Rental(Movie("", 0), 0)
+        customer.addRental(rental)
+
+        val statement = customer.statement()
+        assertEquals(
+                """Rental Record for null
+	2.0()
+Amount owed is 2.0
+You earned 1 frequent renter pointers""",
+                statement)
     }
 }
