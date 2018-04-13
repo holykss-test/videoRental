e110b0ec2502ce942e6af32f985ed2858171318a
Ian <developer.kss@gmail.com>
2018-04-13 17:07:00 +0900

    inline

--- src/main/java/step1/ChildrensMovie.java
+++ src/main/java/step1/ChildrensMovie.java
@@ -4,4 +4,15 @@ public class ChildrensMovie extends Movie {
     public ChildrensMovie(String title) {
         super(title, Movie.CHILDRENS);
     }
+
+    @Override
+    double getPrice(int daysRented) {
+        double thisAmount = 0;
+
+        thisAmount += 1.5;
+        if (daysRented > 3)
+            thisAmount += (daysRented - 3) * 1.5;
+        
+        return thisAmount;
+    }
 }
