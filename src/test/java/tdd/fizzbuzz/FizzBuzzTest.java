ed6261ce4bc1b2fac187c4dd7a0512bcd1a66537
Ian <developer.kss@gmail.com>
2018-04-13 11:01:08 +0900

    실패

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -10,4 +10,9 @@ public class FizzBuzzTest {
     public void fizzbuzz_1_should_says_1() {
         assertEquals("1", FizzBuzz.says(1));
     }
+
+    @Test
+    public void fizzbuzz_2_should_says_2() {
+        assertEquals("2", FizzBuzz.says(2));
+    }
 }
