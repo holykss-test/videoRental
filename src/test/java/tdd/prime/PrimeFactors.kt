f18d532d146ec68e4c56613571e7b401c1458255
Ian <developer.kss@gmail.com>
2018-04-13 13:38:21 +0900

    of

---
+++ src/test/java/tdd/prime/PrimeFactors.kt
@@ -0,0 +1,8 @@
+package tdd.prime
+
+object PrimeFactors {
+    fun of(number: Int): List<Int> {
+        return listOf(number)
+    }
+
+}
---
+++ src/test/java/tdd/prime/PrimeFactorsTest.kt
@@ -0,0 +1,20 @@
+package tdd.prime
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class PrimeFactorsTest {
+    @Test
+    fun primeFactorsOfPrimeNumbers() {
+        assertEquals(listOf(2), PrimeFactors.of(2))
+        assertEquals(listOf(3), PrimeFactors.of(3))
+    }
+
+
+    @Test
+    fun primeFactorsOf4() {
+        assertEquals(listOf(4), PrimeFactors.of(4))
+    }
+
+
+}
