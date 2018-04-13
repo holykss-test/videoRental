6bcadf46a13fa8f5c78cdae9dedffdc0e03ccff3
Ian <developer.kss@gmail.com>
2018-04-13 11:02:26 +0900

    공통 요소 찾기

--- src/test/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,8 +4,8 @@ public class FizzBuzz {
     public static String says(int number) {
 
         if (number == 2)
-            return "2";
+            return "" + 2;
 
-        return "1";
+        return "" + 1;
     }
 }
