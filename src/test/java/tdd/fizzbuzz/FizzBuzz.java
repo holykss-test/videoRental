f790941423416490b160defe3a5b80bd8818c4b8
Ian <developer.kss@gmail.com>
2018-04-13 11:04:40 +0900

    중복 제거

--- src/test/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,6 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public static String says(int number) {
 
-        if (number == 2)
-            return "" + number;
-        if (number == 1)
-            return "" + number;
-
-        throw new RuntimeException("허용되지 않은 입력 값");
+        return "" + number;
     }
 }
